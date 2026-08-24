f3f78b381545d697b65dedeec25a3c076ef54213
erfanhadi <efn1385@gmail.com>
2026-08-24 22:33:23 +0330

    feat : add accordion
    +set onclick for Accordion items
    +add isOpen state for show or hide items content
    +add conditional rendering for items content

--- src/Components/Common/Accordion/Item.jsx
+++ src/Components/Common/Accordion/Item.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import { BiChevronLeft } from "react-icons/bi";
 
 const AccordionItem = ({ label, content }) => {
+  const [isOpen , setIsOpen] = useState(false);
+
+  const toggleIsOpen = () => setIsOpen((prev) => !prev);
+
   return (
     <div>
       <div
+        onClick={toggleIsOpen}
         className={`flex-between select-none h-12 text-sm px-4 flex-ic duration-300 hover:bg-slate-50 active:*:scale-95 min-h-20 group cursor-pointer`}
         tabIndex="1"
       >
@@ -11,7 +17,7 @@ const AccordionItem = ({ label, content }) => {
         <BiChevronLeft />
       </div>
       <div className={`duration-300 transition-all text-sm  text-slate-500`}>
-        {/* Content */}
+        {isOpen && content}
       </div>
     </div>
   );
