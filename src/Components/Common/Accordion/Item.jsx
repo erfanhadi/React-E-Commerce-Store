75a4e0721a973c0c95a513fcdf2a7a2819fb92ed
erfanhadi <efn1385@gmail.com>
2026-08-25 21:56:54 +0330

    style: add Accordion styles and animations
    +add rotate animation for accordion icons
    +add styles for handle visibility of content and delete conditional rendering for content

--- src/Components/Common/Accordion/Item.jsx
+++ src/Components/Common/Accordion/Item.jsx
@@ -10,14 +10,14 @@ const AccordionItem = ({ label, content }) => {
     <div>
       <div
         onClick={toggleIsOpen}
-        className={`flex-between select-none h-12 text-sm px-4 flex-ic duration-300 hover:bg-slate-50 active:*:scale-95 min-h-20 group cursor-pointer`}
+        className={`flex-between select-none h-12 text-sm px-4 flex-ic duration-300 hover:bg-slate-50 active:*:scale-95 min-h-20 group cursor-pointer ${isOpen ? "bg-blue-500/15! text-blue-500!" : ""}`}
         tabIndex="1"
       >
         <div className="duration-300 cursor-pointer w-full">{label}</div>
-        <BiChevronLeft />
+        <BiChevronLeft className={`${isOpen ? "-rotate-90" : "" } duration-300 text-3xl`} />
       </div>
-      <div className={`duration-300 transition-all text-sm  text-slate-500`}>
-        {isOpen && content}
+      <div className={`${isOpen ? "h-full px-10 py-6" : "h-0! opacity-0 invisible overflow-hidden p-0! whitespace-nowrap"} duration-300 transition-all text-sm  text-slate-500`}>
+        {content}
       </div>
     </div>
   );
