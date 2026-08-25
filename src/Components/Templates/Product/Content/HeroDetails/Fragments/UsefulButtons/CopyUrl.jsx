754526fce857a55e45e0a77fb3d1a18f35e926ba
erfanhadi <efn1385@gmail.com>
2026-08-25 22:26:07 +0330

    feat : add logic of copy url
    +add state for control the status of copied or no and write some conditional rendering for better look

--- src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/CopyUrl.jsx
+++ src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/CopyUrl.jsx
@@ -1,10 +1,29 @@
 import { FaShare } from "react-icons/fa6";
 import Tooltip from "../../../../../../Ui/Tooltip";
+import { useState } from "react";
 
 const CopyUrl = () => {
+
+  const [isCopied , setIsCopied] = useState(false);
+
+  const copyUrl = () => {
+    const origin = location.origin;
+    const pathName = location.pathname;
+    const url = origin + pathName;
+
+    navigator.clipboard.writeText(url);
+
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 2000);
+  }
+
   return (
-    <Tooltip text="اشتراک گذاری">
-      <button className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-slate-700">
+    <Tooltip text={`${isCopied ? "کپی شد✅" : "اشتراک گذاری"}`}>
+      <button className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300
+      rounded-lg text-slate-700" onClick={copyUrl}>
         <FaShare />
       </button>
     </Tooltip>
