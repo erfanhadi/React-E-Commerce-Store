5a3dfa94d4fe9e7f3ce346fbb32c8b4bc05374c0
erfanhadi <efn1385@gmail.com>
2026-09-09 20:38:44 +0330

    feat : make nav items interactive

--- src/Components/Common/Sidebar/fragments/NavItem/index.jsx
+++ src/Components/Common/Sidebar/fragments/NavItem/index.jsx
@@ -1,7 +1,6 @@
-import { icon } from 'leaflet'
-import { NavLink } from 'react-router'
+import { Link, NavLink } from 'react-router'
 
-function index({label = "", href , icon , bgColor , iconColor}) {
+function index({ label = "", href, icon, bgColor, iconColor }) {
     return (
         <NavLink
             to={href}
@@ -9,12 +8,28 @@ function index({label = "", href , icon , bgColor , iconColor}) {
                 "flex items-center gap-3 duration-150 transition-all hover:bg-zinc-100/70 p-1.5 rounded-lg relative"
             }
         >
-            <div className={`size-10 rounded-lg flex items-center justify-center 
-                text-xl relative overflow-hidden ${iconColor}`}>
-                <span className={`size-full absolute z-0 opacity-15 inset-0 ${bgColor}`}></span>
-                {icon}
-            </div>
-            <span className="text-sm text-zinc-600 ">{label}</span>
+
+            {Link => (
+                <>
+                    {
+                        Link.isActive && (
+                            <>
+                            <div className={`absolute size-full right-0 rounded-lg opacity-6 ${bgColor}`}></div>
+
+                            <span className={`absolute top-0 w-1 rounded-full h-[80%] bottom-0 my-auto -right-1 z-0 opacity-50 ${bgColor}`}></span>
+                            </>
+                        )
+                    }
+
+                    <div className={`size-10 rounded-lg flex items-center justify-center 
+                        text-xl relative overflow-hidden ${iconColor}`}>
+                        <span className={`size-full absolute z-0 opacity-15 inset-0 ${bgColor}`}></span>
+                        {icon}
+                    </div>
+                    <span className="text-sm text-zinc-600 ">{label}</span>
+
+                </>
+            )}
         </NavLink>
     )
 }
