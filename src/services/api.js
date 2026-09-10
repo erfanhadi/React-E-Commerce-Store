fd4bd3a9d9f6845beb0bdf24fed3ae51c3cdd05c
erfanhadi <efn1385@gmail.com>
2026-09-10 14:22:04 +0330

    fix : fix api config

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://shopino.iran.liara.run/v1",
+    baseURL: "https://shopino.iran.liara.run/v1",
     withCredentials: true,
 
-    timeout: 5000
+    headers: {
+        "Content-Type": "application/json",
+    },
+
+    timeout: 5000,
 });
 
 export default api;
