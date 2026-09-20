deb6e090a586640c086fa2263d6182120d2690a3
erfanhadi <efn1385@gmail.com>
2026-09-20 23:10:04 +0330

    feat : develop and & logic of nested categories

--- src/Components/Templates/Dashboard/common/ProductDrawer/CascadeCategories.jsx
+++ src/Components/Templates/Dashboard/common/ProductDrawer/CascadeCategories.jsx
@@ -1,19 +1,31 @@
-function CascadeCategories({ categories }) {
-    const levels = [categories];
-    console.log(levels);
+import { useState } from "react";
 
-    const handleSelect = () => {
-        //codes
+function CascadeCategories({ categories }) {
+    const [path , setPath] = useState([]);
+    
+    const levels = [categories , ...path.map(cat => cat.subCategories || [] )];
+
+    const handleSelect = (levelIndex , id) => {
+        const options = levels[levelIndex];
+        const node = options.find((category) => category._id === id);
+
+        const newPath = [...path.slice(0 , levelIndex), node];
+        setPath(newPath);
     };
 
     return (
         <div className="space-y-3">
             {levels.map((options, index) => {
+
+                if (!options || options.length === 0) {
+                    return null;
+                }
+
                 return (
                     <select
                         key={index}
-                        // value={ }
-                        onChange={e => handleSelect}
+                        value={path[index]?._id || ""}
+                        onChange={(e) => handleSelect(index , e.target.value)}
                         className="w-full h-10 text-sm rounded-md outline-none primary-border px-3 bg-white"
                     >
                         <option value="" disabled>
