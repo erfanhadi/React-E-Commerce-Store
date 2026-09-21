48e8cd63596f29b5159b5636cc44b5d286f0a8dc
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2026-09-21 15:55:26 +0330

    fix(imports): Fix main and fonts import
    - Fix fonts import by removing the non-assest public import and
    importing it in index.html with link
    - Fix index.css asset import by moving to src/ and importing it from
    main.jsx - Also fixes the tailwindcss import error

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./../public/css/index.css";
-import "./../public/css/fonts.css";
 import { RouterProvider } from "react-router";
 import router from "./lib/routes.jsx";
+import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
